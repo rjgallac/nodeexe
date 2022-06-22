215666077c2cf580aa549296eb60310703e0c3d0
rob <rjgallac@hotmail.com>
2022-06-22 22:37:37 +0100

    add native

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-var exec = require('child_process').execFile;
-
-var fun = function(){
-//    console.log("fun() start");
-   exec("nativedemo.exe --postCode=adsf", function(err, data) {  
-        // console.log(err)
-        console.log("HERE")
-        console.log(data.toString());                       
-    });  
-}
-fun();
+const address = require('./src/formatAddress.js');
+const structuredAddress = {"postcode":"s1 4up"}
+address.format(structuredAddress)
---
+++ src/formatAddress.js
@@ -0,0 +1,14 @@
+var exec = require('child_process').execFile;
+const path = require('path');
+
+module.exports.format = function(address){
+    const demo = path.resolve('nativedemo.exe');
+    cmd = 
+    exec(demo, [`--postCode=${address.postcode}`], function(err, stdout, stderr) {  
+            if (err) {
+                console.error(`exec error: ${err}`);
+                return;
+            }
+            console.log(`${stdout}`);                       
+        });  
+}
